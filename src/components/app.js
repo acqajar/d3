86eae16a2c7d8322ba0ba980cfd7cd4402a120a8
Arsames Qajar <arsamesqajar@Arsamess-MacBook-Pro.local>
2018-03-30 11:57:13 -0700

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <p>React simple starter.</p>
+            </div>
+    );
+  }
+}
+
+
+export default App
